3bec23f36f4a3179599e7c60aa019488a7442b0e
andreas jansson <andreas.s.t.jansson@gmail.com>
2013-04-14 23:28:39 +0100

    enqueue mp3 paths

---
+++ enqueue_mp3_paths.py
@@ -0,0 +1,20 @@
+import job
+import sys
+import s3
+import os
+
+def main(job_name):
+    publisher = job.Job(job_name)
+
+    mp3_paths = s3.ls('andreasjansson', 'makams/mp3')
+
+    for path in mp3_paths:
+        path = s3.url('andreasjansson', path)
+        data = {
+            'path': path
+        }
+        print 'Adding to queue: %s' % path
+        publisher.put_data(data)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
